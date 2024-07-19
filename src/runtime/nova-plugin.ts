428544ec1e1a1990bb92123dd1a01db5e649bfd7
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-07-19 02:56:02 -0400

    feat: runtime and nova plugin def

---
+++ src/runtime/nova-plugin.ts
@@ -0,0 +1,64 @@
+import { defineNitroPlugin, useRuntimeConfig } from "nitro/runtime";
+import type { NovaRuntimeDefinition } from "../types";
+import { consola } from "consola";
+
+type Awaitable<T> = T | Promise<T>;
+
+/**
+ * Defines a Nova plugin for the Nitro runtime.
+ * @param {NovaRuntimeDefinition<Config, T, InitializeReturn, BeforeReturn, AfterReturn>} options - Plugin configuration
+ * @returns {ReturnType<typeof defineNitroPlugin>} Configured Nitro plugin
+ */
+export function defineNovaPlugin<
+  Config extends Record<string, string>,
+  T = any,
+  InitializeReturn extends Awaitable<Record<string, any>> = any,
+  BeforeReturn extends Awaitable<Record<string, any>> = any,
+  AfterReturn extends Awaitable<Record<string, any>> = any
+>({
+  initialize,
+  before,
+  after,
+  runtimeSetup,
+}: NovaRuntimeDefinition<
+  Config,
+  T,
+  InitializeReturn,
+  BeforeReturn,
+  AfterReturn
+>) {
+  return defineNitroPlugin(async (nitro) => {
+    const config = useRuntimeConfig();
+
+    try {
+      const initResult = await initialize(nitro, config);
+      const beforeResult = await before(nitro, initResult);
+
+      await initializeFeatures(runtimeSetup, nitro);
+
+      await after(nitro, beforeResult);
+      consola.success("Nova plugin initialized.");
+    } catch (error) {
+      consola.error("Failed to initialize Nova plugin:", error);
+      throw error;
+    }
+  });
+}
+
+/**
+ * Initializes features based on the runtime setup.
+ *
+ * @param runtimeSetup - The runtime setup configuration
+ * @param nitro - The Nitro instance
+ */
+async function initializeFeatures(
+  runtimeSetup: NovaRuntimeDefinition<any, any, any, any, any>["runtimeSetup"],
+  nitro: any
+) {
+  if (!runtimeSetup) return;
+
+  for (const featureConfig of Object.values(runtimeSetup)) {
+    const handlers = featureConfig.getVirtualHandlers();
+    await featureConfig.initFeatureHandlers(nitro, handlers);
+  }
+}
---
+++ src/runtime/setup-feature.ts
@@ -0,0 +1,29 @@
+import type { NitroApp } from "nitro/types";
+import type { FeatureDefinition } from "../types";
+
+interface InitRuntimeFeatureParams<T, R> {
+  nitro: NitroApp;
+  def: FeatureDefinition;
+  getHandlers: () => T[];
+  setupHandler: (nitro: NitroApp, handler: T) => Promise<R> | R;
+}
+
+/**
+ * Initializes a runtime feature by setting up its handlers.
+ * @param params The parameters for initializing the runtime feature.
+ */
+export async function initRuntimeFeature<T, R>({
+  nitro,
+  def,
+  getHandlers,
+  setupHandler,
+}: InitRuntimeFeatureParams<T, R>): Promise<void> {
+  const handlers = getHandlers();
+  for (const handler of handlers) {
+    try {
+      await setupHandler(nitro, handler);
+    } catch (error) {
+      console.error(`Error setting up ${def.name} handler:`, error);
+    }
+  }
+}
