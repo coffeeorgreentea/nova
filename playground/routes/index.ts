ff3059a8f949047339ca76dc85cfc1d0fb622a9e
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-08-08 03:15:26 -0400

    chore: Update unfinished build script in stripe package.json

--- playground/routes/index.ts
+++ playground/routes/index.ts
@@ -1,8 +1,13 @@
 import { useNitroApp } from "nitro/runtime";
 import { defineEventHandler } from "h3";
+import type { NovaFeatures } from "@gtc-nova/kit";
+
+type Test = NovaFeatures["nats"]["publishers"]["/userEvents"];
 
 export default defineEventHandler(async (event) => {
   const nitroApp = useNitroApp();
+
+  nitroApp.hooks.hook("nova:nats:before", (nitroApp, payload) => {});
 
   return "Hello, world!";
 });
