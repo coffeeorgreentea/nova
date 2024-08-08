0a89e6a716fd0e7a0862495ded4c78d3f9344593
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-08-08 02:39:02 -0400

    WIP: 6692001 chore: Update build.config.ts to include new config entry

--- playground/nitro.config.ts
+++ playground/nitro.config.ts
@@ -1,10 +1,7 @@
 import { defineNitroConfig } from "nitro/config";
-import { natsModule } from "@gtc-nova/nats";
-import { stripeModule } from "@gtc-nova/stripe";
 
 export default defineNitroConfig({
   compatibilityDate: "2024-08-04",
-  modules: [natsModule, stripeModule],
   runtimeConfig: {
     stripe: {
       apiKey: "",
